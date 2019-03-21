d349f120c719442224c63aee72fb31a929986f50
Sam McKelvie <sammck@gmail.com>
2019-03-21 10:52:56 -0700

    comment out in progress opencv normalization code

--- src/main/java/frc/robot/lib/trajectory/jetsoninterface/model/Calib.java
+++ src/main/java/frc/robot/lib/trajectory/jetsoninterface/model/Calib.java
@@ -3,12 +3,20 @@ package frc.robot.lib.trajectory.jetsoninterface.model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import org.opencv.core.MatOfDouble;
+
 public class Calib {
     public double dist[][];
     public double mtx[][];
     public double ret;
     public double rvecs[][][];
     public double tvecs[][][];
+
+    // public List<Mat> cvRvecs;
+    // public List<Mat> cvTvecs;
+    // public MatOfDouble cvMtx;
+    // public MatOfDouble cvDist;
+
 
     @JsonCreator
     public Calib(
@@ -23,5 +31,31 @@ public class Calib {
         this.ret=ret;
         this.rvecs=rvecs;
         this.tvecs=tvecs;
+
+        /*
+        if (mtx != null) {
+            cvMtx = new Mat(3,3,CvType.CV_32FC1);
+            for (int r = 0; r < 3; r++) {
+                for (int c = 0; c < 3; c++) {
+                    double[] v = new double[1];
+                    v[0] = mtx[r][c]
+                    cvMtx.put(r, c, v)
+                }
+            }
+        }
+        if (rvecs != null) 
+            cvRvecs = new ArrayList<>();
+            for (int i = 0; i < rvecs.length; i++) {
+                double rv[][] = rvecs[i];
+                Mat item = MatOfDouble()
+                cvRvecs.add(item);
+            }
+        cvTvecs = new ArrayList<>();
+        */
+
+
+
     }
+
+
 }
